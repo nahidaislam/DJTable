abc5f871235c8c36630faee991bdb3383a9d9227
Nahida Islam <nahida@kth.se>
2019-10-13 19:58:03 +0200

    scroll up button and script added

---
+++ assets/js/scroll.js
@@ -0,0 +1,19 @@
+//Get the scroll icon
+var scrollIcon = document.getElementById("scroll");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollIcon.style.display = "block";
+  } else {
+    scrollIcon.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
